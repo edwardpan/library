93347c7ad6d92221310984cc4483e84f5faffa99
edwardpan <edwardpan@bff33f99-a5fa-4445-b2a2-76045210a35e>
2011-07-18 08:57:44 +0000

    一些杂七杂八不重要的改动和测试
    git-svn-id: svn://www.firecoder.net/main@15 bff33f99-a5fa-4445-b2a2-76045210a35e

---
+++ FireCoder/test/net/firecoder/test/actions/ExpTest.java
@@ -0,0 +1,24 @@
+package net.firecoder.test.actions;
+
+import org.junit.Test;
+
+import com.woobsoft.expressionhelper.ExpressionHelper;
+
+
+public class ExpTest {
+	@Test
+	public void test() {
+//		double r = 0.00055d + 0.0001d;
+////		BigDecimal d = new BigDecimal("0.0002").add(new BigDecimal("0.0001"));
+//		System.out.println(r);
+//		BigDecimal d = new BigDecimal(r);
+//		BigDecimal result = d.setScale(4, BigDecimal.ROUND_HALF_UP);
+//		r = Double.valueOf(result.toString());
+//		System.out.println(r);
+		
+		String exp = "[EXP]$mathToRoundUp(0.00055d + 0.0001d, 4)[/EXP]";
+		ExpressionHelper helper = ExpressionHelper.getInstance();
+		helper.setFullExpStr(exp);
+		System.out.println(helper.executeFullStr());
+	}
+}
